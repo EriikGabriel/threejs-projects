70ba2df9b1ca8151a7d5202d1f5b681f029d36e6
EriikGabriel <erikgabriel.lins@hotmail.com>
2022-08-10 15:44:25 -0300

    Commit - Try fix project dependencies

--- src/models/engine/main.js
+++ src/models/engine/main.js
@@ -1,21 +1,32 @@
-import Game from './script.js'
+import Game from "./script.js";
 
 ((doc, win) => {
-    var playing = play.value
+  var playing = play.value;
 
-    if(playing === 'true') {
-        Game()
-    }
+  if (playing === "true") {
+    Game();
+  }
 
-    jogar.addEventListener('click', () => {
-        initial.style.display = 'none'
-        blocker.style.display = 'initial'
-        play.value = 'true'
-        Game()
-    })
+  jogar.addEventListener("click", () => {
+    initial.style.display = "none";
+    blocker.style.display = "initial";
+    play.value = "true";
+    Game();
+  });
 
-    sair.addEventListener('click', () => {
-        win.history.back()
-    })
-    
+  creditos.addEventListener("click", () => {
+    const menu = doc.getElementById("menu");
+
+    menu.style.display = "none";
+  });
+
+  voltarMenu.addEventListener("click", () => {
+    const menu = doc.getElementById("menu");
+
+    menu.style.display = "flex";
+  });
+
+  sair.addEventListener("click", () => {
+    win.history.back();
+  });
 })(document, window);
